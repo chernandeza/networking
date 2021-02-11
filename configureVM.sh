154167f1d7496821a1149c932aee4493964f3153
Carlos Hernández A <chernandeza@users.noreply.github.com>
2021-02-11 09:42:25 -0600

    Update configureVM.sh

--- configureVM.sh
+++ configureVM.sh
@@ -1,1 +1,20 @@
-!/bin/bash
+#!/bin/bash
+
+# Update repository information and packages
+sudo apt-get update 
+sudo apt-get upgrade 
+
+########################
+### Install software ###
+########################
+
+## Basic tools
+## Midnight Commander ##
+sudo apt-get install mc 
+
+################################
+### Settings and preferences ###
+################################
+
+
+
