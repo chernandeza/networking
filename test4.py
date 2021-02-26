0d670e4f6179603c4c835a493fbe5fa1c021fea6
Carlos Hernández A <chernandeza@users.noreply.github.com>
2021-02-25 22:19:27 -0600

    Update test4.py

--- test4.py
+++ test4.py
@@ -7,22 +7,32 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from csv import reader
 
-# Create results file
+# Create results file and add Header row
 with open('results.csv', 'w') as f1:
     writer = csv.writer(f1, delimiter='\t', lineterminator='\n', quoting=csv.QUOTE_ALL)
     writer.writerow(["Domain", "TLSVersion", "CommonName", "WildcardCN", "CertIssuer", "SAN"])
     f1.close()
 
-# open file in read mode
+# open input file in read mode
 with open('domains.csv', 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = reader(read_obj)
     # Iterate over each row in the csv using reader object
     for row in csv_reader:
         try:
+            # Variable declaration and cleanup #
+            tlsVersion = "NULL"
+            commonName = "NULL"
+            certIssuer = "NULL"
+            san_dns_names = "NULL"
+            # --- First, we validate TLS protocol version --- #
             # row variable is a list that represents a row in csv
-            wildcardCN = False  # Tells if "*" was found on either CN or SAN
-            domain = row.pop()
+            wildcardCN = False  # True if "*" was found on either CN or SAN
+            url: str = row.pop()
+
+            # Removes everything after first "/" is found
+            domain = url.split("/", 1)[0]
+
             print("Evaluating -> " + domain)
             context: SSLContext
             context = ssl.create_default_context()
@@ -32,40 +42,58 @@ with open('domains.csv', 'r') as read_obj:
             # print('Selected version by the server: ', sslSocket.version())
             tlsVersion = sslSocket.version()  # Extracts TLS version used to establish connection.
             sslSocket.close()
-            certificate: bytes = ssl.get_server_certificate((domain, 443)).encode('utf-8')  #
+
+            # --- Now, validate certificate properties --- #
+            # Obtains certificate from connection
+            certificate: bytes = ssl.get_server_certificate((domain, 443)).encode('utf-8')
             loaded_cert = x509.load_pem_x509_certificate(certificate, default_backend())
+            # Gets CN from certificate #
             common_name = loaded_cert.subject.get_attributes_for_oid(x509.oid.NameOID.COMMON_NAME)
+
+            # Gets Issuer name from certificate #
             issuer = loaded_cert.issuer.get_attributes_for_oid(x509.oid.NameOID.COMMON_NAME)
+            # print("*** Issuer ***")
+            certIssuer = issuer.pop().value
+            # print(certIssuer)
+
             # print("*** Common Name ***")
             # print(common_name[0].value)
             commonName = common_name[0].rfc4514_string()
-            if "*" in commonName:
+            if "*" in commonName:  # Search for wildcard on Common Name
                 wildcardCN = True
-                print("Found wildcard CN")
-                print(commonName)
-            # print("*** Issuer ***")
-            # print(issuer.pop().value)
-            certIssuer = issuer.pop().value
-            print(certIssuer)
+                print("... Domain is using wildcard CN")
+                # print(commonName)
+
+            # Gets SAN from certificate attributes
             san = loaded_cert.extensions.get_extension_for_class(x509.SubjectAlternativeName)
             san_dns_names = san.value.get_values_for_type(x509.DNSName)
             # print("*** SAN ***")
             # print(san_dns_names)
-            if any("*" in s for s in san_dns_names):
+            if any("*" in s for s in san_dns_names): # Search for wildcard on SAN List
                 wildcardCN = True
-                print("Found wildcard SAN")
+                print("... Domain is using wildcard SAN")
             with open('results.csv', 'a+') as f1:
                 writer = csv.writer(f1, delimiter='\t', lineterminator='\n', quoting=csv.QUOTE_ALL)
                 writer.writerow([domain, tlsVersion, commonName, wildcardCN, certIssuer, san_dns_names])
                 f1.close()
+
         except socket.gaierror:
             print("Host Not Found")
+            with open('results.csv', 'a+') as f1:
+                writer = csv.writer(f1, delimiter='\t', lineterminator='\n', quoting=csv.QUOTE_ALL)
+                writer.writerow([domain, tlsVersion, commonName, wildcardCN, certIssuer, san_dns_names])
+                f1.close()
         except ssl.SSLCertVerificationError:
             print('Could not validate certificate')
+            with open('results.csv', 'a+') as f1:
+                writer = csv.writer(f1, delimiter='\t', lineterminator='\n', quoting=csv.QUOTE_ALL)
+                writer.writerow([domain, tlsVersion, commonName, wildcardCN, certIssuer, san_dns_names])
+                f1.close()
         except ssl.SSLError:
             print("Unknown TLS error")
+            with open('results.csv', 'a+') as f1:
+                writer = csv.writer(f1, delimiter='\t', lineterminator='\n', quoting=csv.QUOTE_ALL)
+                writer.writerow([domain, tlsVersion, commonName, wildcardCN, certIssuer, san_dns_names])
+                f1.close()
 
 print("Validation complete...")
-
-
-
